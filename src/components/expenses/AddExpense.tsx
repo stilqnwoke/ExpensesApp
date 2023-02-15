311a65accbf52066d8a1f8b4a61ea6ae18f42c74
stilqnwoke <69080350+stilqnwoke@users.noreply.github.com>
2023-02-15 18:09:46 +0200

    Added Material UI library

--- src/components/expenses/AddExpense.tsx
+++ src/components/expenses/AddExpense.tsx
@@ -1,13 +1,27 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { ExpenseContext } from "../../store/expenses-reducer.tsx";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import InputAdornment from "@mui/material/InputAdornment";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 
 const AddExpense = () => {
   const { addExpense } = useContext(ExpenseContext);
   const nameRef = useRef(null);
   const amountRef = useRef(null);
+  const [budgetCategory, setBudgetCategory] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setBudgetCategory(event.target.value);
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
+
     addExpense({
       name: nameRef.current.value,
       amount: amountRef.current.value,
@@ -16,11 +30,66 @@ const AddExpense = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input ref={nameRef} required />
-        <input ref={amountRef} required />
-        <button type="submit">Add</button>
-      </form>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "24ch" },
+          border: "1px dashed grey",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: 300,
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          required
+          id="outlined-required"
+          ref={nameRef}
+          label="Description"
+          variant="outlined"
+          size="small"
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">BGN</InputAdornment>,
+          }}
+          ref={amountRef}
+          label="Amount"
+          variant="outlined"
+          size="small"
+        />
+
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel id="demo-simple-select-helper-label">
+            Budget Category
+          </InputLabel>
+
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={budgetCategory}
+            label="Budget Category"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button variant="contained" size="medium">
+          Add Expense
+        </Button>
+      </Box>
     </div>
   );
 };
